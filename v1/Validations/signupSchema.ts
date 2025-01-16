2b956f5c2ee0e2e63bdddad6f90db01586715ad3
harshil-sinha <130955042+harshil-sinha@users.noreply.github.com>
2025-01-16 17:13:39 +0530

    Update signupSchema.ts

--- v1/Validations/signupSchema.ts
+++ v1/Validations/signupSchema.ts
@@ -1,12 +1,12 @@
-import * as Joi from "joi";
+import Joi from "joi";
 
 const signupSchema = Joi.object({
-  first_name: Joi.string().min(2).max(50).required().trim(),
-  last_name: Joi.string().min(2).max(50).required().trim(),
+  first_name: Joi.string().min(2).max(50).required().trim().replace(/'/g, "''"),
+  last_name: Joi.string().min(2).max(50).required().trim().replace(/'/g, "''"),
   gender: Joi.string().valid("Male", "Female", "Other").required().trim(),
   dob: Joi.date().iso().required(),
   email: Joi.string().email().required().trim(),
-  password: Joi.string().min(6).required().trim(),
+  password: Joi.string().min(8).required().trim().replace(/'/g, "''"),
   mobile: Joi.string()
     .pattern(/^[0-9]{10}$/)
     .required()
